186b055c2afa0aecfc25a6cab6736082bb10c37d
Tim <tim.cartr@gmail.com>
2018-06-18 15:28:08 -0600

    First attempt through #7

--- assessment.js
+++ assessment.js
@@ -1,34 +1,37 @@
 // #1 create a variable called 'myName' and assign it to your name as a string
 
 // CODE HERE
-
+var myName = "Tim";
 // #2 create a variable called 'faveNum' and assign it to your favorite number
 
 // CODE HERE
-
+var faveNum = 21;
 // #3 create a variable called 'lovesCode' and set it to true
 
 // CODE HERE
-
+var lovesCode = true;
 // #4 Using the previously created variables, create a new variable called 'me' that uses the myName and faveNum varible to create a sentence like this 'My name is Bryan and my favorite number is 3'
 
 // CODE HERE
-
+var me = "My name is " + myName + "and my favortie number is " + faveNum;
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. *trueFaveNum* should equal true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator
 
 // CODE HERE
-
+var trueFaveNum;
+trueFaveNum == faveNum;
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
 
 // CODE HERE
-
+var notHakeem;
+notHakeem != myName;
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
 var age = 18;
 var canDrive;
 
-
 // CODE HERE
-
+if (age>=16){
+  canDrive=true
+}else {canDrive=false};
 // #8 Create an if statement inside of the responseCreator function that sets the colorResponse variable value based off of a passed in value. The param value passed in will be added in the unit test. If the param value is 'green' set colorResponse variable to 'Green is okay'. If the param value is 'red' set reponse variable to 'I heart red'. Otherwise set the colorResponse variable to 'What is your favorite color?'. Make sure to return the colorResponse variable at the end once it has been properly set.
 
 var colorResponse;
